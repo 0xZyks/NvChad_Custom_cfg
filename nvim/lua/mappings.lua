c7bed3b79194150a89a93b2788c0a9551bf3b820
Théo SIGNORI <theosignori@MacBook-Air-de-Theo.local>
2025-12-07 05:00:49 +0100

    Adding ALT+Right/Left too switch buffertab

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -10,18 +10,30 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+
 map("n", "<F1>", function()
-  if vim.api.nvim_buf_line_count(0) <= 1 then
-    vim.cmd("Stdheader")
-  end
+	if vim.api.nvim_buf_line_count(0) <= 1 then
+		vim.cmd("Stdheader")
+	end
 end, { desc = "Add 42 Header" })
+
 -- Mapping F3 en VISUAL pour toggle /* ... */
 -- VISUAL: F3 -> toggle block comment sur la sélection
 vim.keymap.set("v", "<F3>", function()
-  local start_line = vim.fn.line("v") -- ligne où le Visual a commencé
-  local end_line = vim.fn.line(".")   -- ligne du curseur actuel
-  require("comments_block").toggle_block_comment(start_line, end_line)
+	local start_line = vim.fn.line("v") -- ligne où le Visual a commencé
+	local end_line = vim.fn.line(".")   -- ligne du curseur actuel
+	require("comments_block").toggle_block_comment(start_line, end_line)
 end, { desc = "Toggle block comment (visual selection)" })
+
+-- ALT + Left  → buffer précédent
+vim.keymap.set("n", "<A-Left>", function()
+	require("nvchad.tabufline").prev()
+end, { desc = "Previous buffer" })
+-- ALT + Right → buffer suivant
+vim.keymap.set("n", "<A-Right>", function()
+	require("nvchad.tabufline").next()
+end, { desc = "Next buffer" })
+
 -- Toggle terminal flottant avec Ctrl+\
 map({ "n", "t" }, "<C-\\>", "<cmd>ToggleTerm<CR>", { desc = "Toggle terminal" })
 
@@ -31,7 +43,7 @@ map("n", "<F4>", "<cmd>ToggleTerm size=15 direction=float<CR>", { desc = "Termin
 -- Terminal vertical
 map("n", "<F5>", "<cmd>ToggleTerm size=10 direction=horizontal<CR>", { desc = "Terminal vertical" })
 -- Raccourci pour revenir au mode normal depuis un terminal-
-vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
+map("t", "<Esc>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
 
 -- Close current BufferTab
 map("n", "<leader>q", "<cmd>bd<CR>", { desc = "Close Current BufferTab"});
